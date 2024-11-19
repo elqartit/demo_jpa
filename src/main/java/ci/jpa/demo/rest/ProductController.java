944ae70cc99d4d000f1221b905e829cd9e56ae88
AKELE Armistice Epiphane <akelearmistice153@gmail.com>
2024-11-19 10:39:19 +0000

    updateCommit

--- src/main/java/ci/jpa/demo/rest/ProductController.java
+++ src/main/java/ci/jpa/demo/rest/ProductController.java
@@ -1,14 +1,18 @@
 package ci.jpa.demo.rest;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ci.jpa.demo.domain.Category;
 import ci.jpa.demo.domain.Product;
+import ci.jpa.demo.repository.CategoryRepository;
 import ci.jpa.demo.repository.ProductRepository;
 
 @RestController
@@ -17,6 +21,9 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+    @Autowired
+    private CategoryRepository categoryRepository;
+
     @GetMapping("/products")
     public List<Product> getProducts() {
         List<Product> products = productRepository.findAll();
@@ -31,9 +38,20 @@ public class ProductController {
         return foundProduct;
     }
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, First rest API";
+    
+    @PostMapping("/product")
+    public Product ediProduct (@RequestBody Product product) {
+        Category category = product.getCategory();
+        categoryRepository.save(category);
+        Product founProduct = productRepository.save(product);
+        System.out.println("produit ajouté:" + founProduct);
+        return founProduct;
     }
 
+    @DeleteMapping("/product/{id}")
+    public String deleteProduct(@PathVariable(name = "id") long id) {
+        System.out.println("Produit à supprimer: " + id);
+        productRepository.deleteById(id);
+        return "Produit à supprimer avec succes";
+    }
 }
